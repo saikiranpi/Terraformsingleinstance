af02287d53ae838e5f9462575fdb57a252a39c75
Pinapathruni Saikiran <109568252+saikiranpi@users.noreply.github.com>
2024-10-11 17:15:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -113,6 +113,15 @@ data "aws_ami" "my_ami" {
 #         Owner = "sai"
 # 	CostCenter = "ABCD"
 #     }
+#      user_data = <<- EOF
+#      #!/bin/bash
+#      	sudo apt-get update
+#      	sudo apt-get install -y nginx
+#      	echo "<h1>${var.env}-Server-1</h1>" | sudo tee /var/www/html/index.html
+#      	sudo systemctl start nginx
+#      	sudo systemctl enable nginx
+#      EOF
+
 # }
 
 ##output "ami_id" {
