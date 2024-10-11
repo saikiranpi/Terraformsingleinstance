8910db1c1bd87c65a95d3bc8ed43018559f53ded
Pinapathruni Saikiran <109568252+saikiranpi@users.noreply.github.com>
2024-10-11 18:14:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -124,6 +124,16 @@ data "aws_ami" "my_ami" {
 
 # }
 
+resource "aws_dynamodb_table" "state_locking" {
+  hash_key = "LockID"
+  name     = "dynamodb-state-locking"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  billing_mode = "PAY_PER_REQUEST"
+}
+
 ##output "ami_id" {
 #  value = "${data.aws_ami.my_ami.id}"
 #}
